c7b176023c7ddebf9e3fa78d0f6f1729469fc549
LuMir <rpfos@naver.com>
2023-08-09 02:54:21 +0900

    add tidory.config.example.js

---
+++ tidory.config.example.js
@@ -0,0 +1,52 @@
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+/**
+ * Tidory Configuration
+ * https://tidory.com/docs/configuration
+ */
+module.exports = {
+  ts_session: '',
+  url: '',
+
+  preview: {
+    /**
+     * homeType
+     *
+     * NONE
+     * COVER
+     */
+    homeType: 'NONE',
+
+    /**
+     * Preview Mode
+     *
+     * index
+     * entry
+     * category
+     * tag,
+     * location
+     * media,
+     * guestbook
+     */
+    mode: 'index',
+  },
+
+  /**
+   * Alias
+   */
+  alias: {
+    '@': 'assets',
+  },
+
+  /**
+   * Webpack Configuration
+   *
+   * @param {object} webpackConfig
+   */
+  extends(webpackConfig) {
+    return {
+      ...webpackConfig,
+      plugins: [new ESLintPlugin(), ...webpackConfig.plugins],
+    };
+  },
+};
