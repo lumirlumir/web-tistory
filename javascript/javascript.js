83ced4d3cb391a4761184375159b7727e3c9096c
LuMir <rpfos@naver.com>
2023-08-12 00:09:21 +0900

    mod views style js(event delegation pattern)

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,104 +1,91 @@
-/* Variables */
-const body = document.querySelector('body');
-const header = document.querySelector('#wrap header');
-const menu = document.querySelector('#wrap header .util .menu');
-const profile = document.querySelector('#wrap header .util .profile');
-const search = document.querySelector('#wrap header .util .search');
-const bodyClass = body.classList;
-const searchClass = search.classList;
-
-/* Event */
-// window - setProperty - scrollY progress
-window.addEventListener('scroll', () => {
-  document.documentElement.style.setProperty(
-    '--scrollRatio',
-    (
-      window.scrollY /
-      (document.body.scrollHeight - window.innerHeight)
-    ).toFixed(4),
-  );
-});
-
-// window - Media Query
-function sideMenu() {
-  if (window.innerWidth > 1366) {
-    bodyClass.add('sideMenu');
-  } else {
-    bodyClass.remove('sideMenu');
+window.addEventListener('load', () => {
+  /* Function */
+  /**
+   * Same as 'document.querySelector' (Idea from jQuery)
+   * @param {string} selector selector
+   * @returns document.querySelector(selector)
+   */
+  function $(selector) {
+    return document.querySelector(selector);
   }
-}
-window.addEventListener('resize', sideMenu);
-document.addEventListener('DOMContentLoaded', sideMenu);
-
-// #wrap header
-header.addEventListener('mouseleave', () => {
-  searchClass.remove('on');
-});
-
-// #wrap header .util .menu
-menu.addEventListener('click', () => {
-  if (bodyClass.contains('sideMenu')) {
-    bodyClass.remove('sideMenu');
-  } else {
-    bodyClass.add('sideMenu');
+  /**
+   * For media query
+   */
+  function sideMenu() {
+    if (window.innerWidth > 1366) {
+      $('body').classList.add('sideMenu');
+    } else {
+      $('body').classList.remove('sideMenu');
+    }
   }
-});
 
-// #wrap header .util .profile
-document.addEventListener('DOMContentLoaded', () => {
+  /* Event Delegation Pattern */
+  // window - load
   if (window.T && window.T.config.USER.name) {
-    profile.querySelector('.login').style.display = 'none';
-    profile.querySelector('.logout').style.display = 'block';
+    $('#login').style.display = 'none';
+    $('#logout').style.display = 'block';
   } else {
-    profile.querySelector('.login').style.display = 'block';
-    profile.querySelector('.logout').style.display = 'none';
+    $('#login').style.display = 'block';
+    $('#logout').style.display = 'none';
   }
-});
-profile.addEventListener('click', () => {
-  if (getComputedStyle(profile.querySelector('nav')).display === 'none') {
-    profile.querySelector('nav').style.display = 'block';
-  } else {
-    profile.querySelector('nav').style.display = 'none';
-  }
-});
-profile.addEventListener('mouseleave', () => {
-  profile.querySelector('nav').style.display = 'none';
-});
-profile.querySelector('.login').addEventListener('click', () => {
-  document.location.href = `https://www.tistory.com/auth/login?redirectUrl=${encodeURIComponent(
-    window.TistoryBlog.url,
-  )}`;
-});
-profile.querySelector('.logout').addEventListener('click', () => {
-  document.location.href = `https://www.tistory.com/auth/logout?redirectUrl=${encodeURIComponent(
-    window.TistoryBlog.url,
-  )}`;
-});
 
-// #wrap header .util .search
-search.addEventListener('click', () => {
-  if (!searchClass.contains('on')) {
-    searchClass.add('on');
-    search.querySelector('input').focus();
-  }
-});
+  sideMenu();
 
-// Keyup ESC
-document.addEventListener('keyup', e => {
-  if (e.key === 'Escape') {
-    searchClass.remove('on');
-    profile.querySelector('nav').style.display = 'none';
-    document
-      .querySelector('.comment-list ul li .author-meta .control .link')
-      .removeAttribute('style');
-  }
-});
+  // window - resize
+  window.addEventListener('resize', () => {
+    sideMenu();
+  });
 
-// $(document).on("click", ".comments .comment-list ul li .author-meta .control button", function(){
-//   if ( $(this).siblings(".link").is(":hidden") ){
-//     $(".comments .link").removeAttr("style");
-//     $(this).siblings(".link").show();
-//   } else {
-//     $(this).siblings(".link").hide();
-//   }
-// });
+  // window - scroll
+  window.addEventListener('scroll', () => {
+    $('html').style.setProperty('--scrollRatio', (window.scrollY / (document.body.scrollHeight - window.innerHeight)).toFixed(4));
+  });
+
+  // document - keyup
+  document.addEventListener('keyup', event => {
+    switch (event.key) {
+      case 'Escape':
+        $('#search').classList.remove('on');
+        $('#profile nav').style.display = 'none';
+        break;
+
+      default:
+        break;
+    }
+  });
+
+  // body - click
+  $('body').addEventListener('click', event => {
+    switch (event.target.id) {
+      case 'menu':
+        $('body').classList.toggle('sideMenu');
+        break;
+
+      case 'profile':
+        $('#profile nav').style.display = getComputedStyle($('#profile nav')).display === 'none' ? 'block' : 'none';
+        $('#profile').addEventListener('mouseleave', () => {
+          $('#profile nav').style.display = 'none';
+        });
+        break;
+
+      case 'login':
+        document.location.href = `https://www.tistory.com/auth/login?redirectUrl=${encodeURIComponent(window.TistoryBlog.url)}`;
+        break;
+
+      case 'logout':
+        document.location.href = `https://www.tistory.com/auth/logout?redirectUrl=${encodeURIComponent(window.TistoryBlog.url)}`;
+        break;
+
+      case 'search':
+        $('#search').classList.toggle('on');
+        $('#search input').focus();
+        $('header').addEventListener('mouseleave', () => {
+          $('#search').classList.remove('on');
+        });
+        break;
+
+      default:
+        break;
+    }
+  });
+});
