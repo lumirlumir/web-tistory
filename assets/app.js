0c6bbeb1140541752baec3320026d0a7bdebe8e5
LuMir <rpfos@naver.com>
2023-08-16 04:34:35 +0900

    add react and mod dev-dependencies

--- assets/app.js
+++ assets/app.js
@@ -11,3 +11,13 @@
  *
  * after import assets it will be contained in app.js
  */
+
+/* React */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from '../components/App';
+import Appt from '../components/Appt';
+
+ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<Appt />, document.getElementById('appt'));
---
+++ components/Appt.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HelloWorld = () => {
+  return <h1>Ohayo, world!</h1>;
+};
+
+export default HelloWorld;
